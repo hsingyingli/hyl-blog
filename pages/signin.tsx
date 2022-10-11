c4af8b84db68d91b5035c88c9a97b83d91ea5140
hsingyingli <xingyingli855@gmail.com>
2022-10-11 13:26:05 +0800

    Sign in/up: SSR to CSR

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,15 +1,25 @@
-import { GetServerSideProps } from "next";
 import Link from "next/link"
 import { useRouter } from 'next/router';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import toast from 'react-hot-toast';
+import Loading from "../components/Loading";
 import supabase from "../utils/supabaseClient";
 
 
 const SignIn: React.FC = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const user = supabase.auth.user()
+
+  useEffect(() => {
+    setIsLoading(true)
+    if (user) {
+      router.push('/')
+    }
+    setIsLoading(false)
+  }, [user])
 
   const handleOnSubmit = async (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault()
@@ -35,14 +45,14 @@ const SignIn: React.FC = () => {
     router.push("/")
   }
 
-  return (
+  return isLoading ? <Loading /> : (
     <div className="min-h-[calc(100vh_-_40px)] flex items-center justify-center">
       <div className="rounded-xl border-2 border-black dark:border-gray-200 p-6 max-w-md w-full">
         <h1 className="text-2xl mb-6 text-center">Sign In</h1>
         <form className="flex gap-3 flex-col" onSubmit={handleOnSubmit}>
           <input className="p-2 rounded-lg border-2 border-gray-500 bg-transparent dark:text-white text-black" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email' autoComplete="false" required />
           <input className="p-2 rounded-lg border-2 border-gray-500 bg-transparent dark:text-white text-black" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' required />
-          <button className="p-2 rounded-lg border-2 border-gray-500 dark:hover:border-white hover:border-black" type='submit'>Submit</button>
+          <button disabled={isLoading} className="p-2 rounded-lg border-2 border-gray-500 dark:hover:border-white hover:border-black" type='submit'>Submit</button>
         </form>
         <hr className="w-full border-[1px] border-black dark:border-white my-6" />
         <p className="text-center">Dont have account yet? <span className="dark:text-cyan-400 text-cyan-600"><Link href="/signup" passHref><a>signup</a></Link></span></p>
@@ -50,23 +60,6 @@ const SignIn: React.FC = () => {
 
     </div>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const { user, error } = await supabase.auth.api.getUserByCookie(req)
-  if (user) {
-    return {
-      redirect: {
-        destination: "/",
-      },
-      props: {}
-    }
-  }
-
-  return {
-    props: {
-    }
-  }
 }
 
 export default SignIn
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,16 +1,27 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useRouter } from 'next/router'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import toast from 'react-hot-toast'
+import Loading from "../components/Loading";
 import supabase from "../utils/supabaseClient";
 
 
 const SignUp: React.FC = () => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(true)
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const user = supabase.auth.user()
+
+  useEffect(() => {
+    setIsLoading(true)
+    if (user) {
+      router.push('/')
+    }
+    setIsLoading(false)
+  }, [user])
 
   const handleOnSubmit = async (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault()
@@ -45,7 +56,7 @@ const SignUp: React.FC = () => {
     router.push('/')
   }
 
-  return (
+  return isLoading ? <Loading /> : (
     <div className="min-h-[calc(100vh_-_40px)] flex items-center justify-center">
       <div className="rounded-xl border-2 border-black dark:border-gray-200 p-6 max-w-md w-full">
         <h1 className="text-2xl mb-6 text-center">Sign Up</h1>
@@ -53,7 +64,7 @@ const SignUp: React.FC = () => {
           <input className="p-2 rounded-lg border-2 border-gray-500 bg-transparent dark:text-white text-black" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' required />
           <input className="p-2 rounded-lg border-2 border-gray-500 bg-transparent dark:text-white text-black" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email' autoComplete="false" required />
           <input className="p-2 rounded-lg border-2 border-gray-500 bg-transparent dark:text-white text-black" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' required />
-          <button className="p-2 rounded-lg border-2 border-gray-500 dark:hover:border-white hover:border-black" type='submit'>Submit</button>
+          <button disabled={isLoading} className="p-2 rounded-lg border-2 border-gray-500 dark:hover:border-white hover:border-black" type='submit'>Submit</button>
         </form>
         <hr className="w-full border-[1px] border-black dark:border-white my-6" />
         <p className="text-center">have account? <span className="dark:text-cyan-400 text-cyan-600"><Link href="/signin"><a >signin</a></Link></span></p>
@@ -61,23 +72,5 @@ const SignUp: React.FC = () => {
     </div>
   )
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const { user, error } = await supabase.auth.api.getUserByCookie(req)
-  if (user) {
-    return {
-      redirect: {
-        destination: "/",
-      },
-      props: {}
-    }
-  }
-
-  return {
-    props: {
-    }
-  }
-}
-
 
 export default SignUp
