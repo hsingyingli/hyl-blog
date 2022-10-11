fc637f0cb1111896ee2986a30d7fb3590d78b4bf
hsingyingli <xingyingli855@gmail.com>
2022-10-11 13:17:28 +0800

    fix theme icon

--- components/ThemeToggleButton/index.tsx
+++ components/ThemeToggleButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import useTheme from '../../hooks/useTheme'
 
@@ -7,7 +7,7 @@ const ThemeToggleButton: React.FC = () => {
 
   return (
     <div className='flex items-center justify-center rounded-lg w-10 h-10 dark:bg-purple-500 dark:hover:bg-purple-600 duration-300 bg-yellow-300 hover:bg-yellow-400 cursor-pointer text-xl' onClick={toggleTheme}>
-      {theme === 'dark' ? <FaMoon /> : <FaSun />}
+      {theme === 'light' ? <FaSun /> : <FaMoon />}
     </div >
   )
 }
